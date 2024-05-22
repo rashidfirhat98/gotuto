10a8fe48f30b62f63b2ab5c30039cebdfc9647b9
rashidridha <rashidfirhat98@gmail.com>
2024-05-22 22:09:13 +0800

    Boolean and conditionals

--- main.go
+++ main.go
@@ -5,28 +5,33 @@ import (
 )
 
 func main() {
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("value of x is:", x)
-	// 	x++
-	// }
+	age := 20
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
 	
-	// for i := 0; i < 5; i++ {
-	// 	fmt.Println("value of i is:", i)
-	// }
-
-	names := []string{"mario", "luigi", "yoshi", "peach"}
-
-	// for i := 0; i < len(names); i++ {
-	// 	fmt.Println(names[i])
-	// }
-
-	for _, value := range names {
-		fmt.Printf("the value is %v \n", value)
-		value = "new string"
-		// value does not alter the original slice because it acts like a local copy 
-		// of the variable
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 40")
 	}
 
-	fmt.Println(names)
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
+
+		fmt.Printf("the value at pos %v is %v \n", index, value)
+	}
 }
