bbac1424aa432c34a541df3995080bbfde900a3f
rashidridha <rashidfirhat98@gmail.com>
2024-05-22 22:47:13 +0800

    Return multiple values

--- main.go
+++ main.go
@@ -2,38 +2,33 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
-func sayGreeting(n string) {
-	fmt.Printf("Good morning %v \n", n)
-}
+func getInitials(n string) (string, string)  {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
 
-func sayBye(n string) {
-	fmt.Printf("Goodbye %v \n", n)
-}
-
-func cycleNames(n []string, f func(string)) {
-	for _, v := range n {
-		f(v)
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1])
 	}
-}
 
-func circleArea(r float64) float64{
-	return math.Pi*r*r
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
+	
 }
 
 func main() {
-	// sayGreeting("mario")
-	// sayGreeting("luigi")
-	// sayBye("mario")
+	fn1, sn1 := getInitials("uzumaki naruto")
+	fn2, sn2 := getInitials("uchiha sasuke")
+	fn3, sn3 := getInitials("gaara")
+	
+	fmt.Println(fn1, sn1)
+	fmt.Println(fn2, sn2)
+	fmt.Println(fn3, sn3)
 
-	cycleNames([]string{"naruto", "sasuke", "sakura"}, sayGreeting)
-	cycleNames([]string{"naruto", "sasuke", "sakura"}, sayBye)
-
-	a1 := circleArea(10.5)
-	a2 := circleArea(15)
-
-	// fmt.Println(a1, a2)
-	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f \n", a1, a2)
 }
