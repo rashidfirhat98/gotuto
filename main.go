a1fb696d7deb86cbddaf384cc72a9b3350604c9f
rashidridha <rashidfirhat98@gmail.com>
2024-05-22 15:25:03 +0800

    Arrays and slices

--- main.go
+++ main.go
@@ -3,27 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	age := 25
-	name := "Rashid"
+	// var ages [3]int = [3]int{20, 25, 30}
+	var ages = [3]int{20, 25, 30}
 
-	// Print
-	fmt.Print("hello, ")
-	fmt.Print("world! \n")
-	fmt.Print("new line \n")
-	
-	// Println
-	fmt.Println("hello ppl")
-	fmt.Println("goodbye ppl")
-	fmt.Println("my age is", age, "and my name is", name)
+	names := [4]string{"yoshi", "mario", "peach", "bowser"}
 
-	// Printf (formatted strings) %_ = format specifier
-	fmt.Printf("my age is %v and my name is %v \n", age, name)
-	fmt.Printf("my age is %q and my name is %q \n", age, name)
-	fmt.Printf("age is of type %T \n", age)
-	fmt.Printf("you scored %f points! \n", 225.55)
-	fmt.Printf("you scored %0.1f points \n", 225.55)
+	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
 
-	// Sprintf (save formatted strings)
-	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
-	fmt.Println("the saved string is:", str)
+	// slices (use arrays under the hood)
+	var scores = []int{100, 50, 60}
+	scores[2] = 25
+	scores = append(scores, 85)
+
+	fmt.Println(scores, len(scores))
+
+	// slices ranges
+	rangeOne := names[1:3]
+	rangeTwo := names[2:]
+	rangeThree := names[:3]
+
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+	rangeOne = append(rangeOne, "koopa")
+
+	fmt.Println(rangeOne)
 }
